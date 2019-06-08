bc8c5e2bedc657e6fc77454086ded773439ade19
生田　真深 <mafutan@icloud.com>
2019-06-08 17:02:05 +0900

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,15 @@
+public class FizzBuzz {
+public static String main(String[] args) {
+FizzBuzz()
+}
+}
+
+public static String FizzBuzz(int number) {
+ if(number % 15 == 0) {
+  return FizzBiuzz;
+ } if(number % 5 == 0) {
+  return Buzz;
+ } if(number % 3 ==0) {
+  return Fizz;
+ } else {
+  return number
